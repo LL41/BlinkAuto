7284712c72bdc6d5e0e917af45fbbe23c502f3d5
Luke <logueluke41@gmail.com>
2023-04-22 21:15:32 +0000

    Chnaged tabs to spaces. Updates time sleep after disarm try.

--- BlinkAuto.py
+++ BlinkAuto.py
@@ -7,58 +7,59 @@ from blinkpy.blinkpy import Blink
 from blinkpy.auth import Auth
 
 def arm_blink(armed):
-	username = 'YOUR_USERNAME'
-	password = 'YOUR_PASSWORD'
-	blink = Blink()
-	auth = Auth({"username": f'{username}', "password": f'{password}'})
-	blink.auth = auth
-	blink.start()
-	blink.sync["YOUR_SYNCMODULE_NAME"].arm = armed
+    username = 'YOUR_USERNAME'
+    password = 'YOUR_PASSWORD'
+    blink = Blink()
+    auth = Auth({"username": f'{username}', "password": f'{password}'})
+    blink.auth = auth
+    blink.start()
+    blink.sync["YOUR_SYNCMODULE_NAME"].arm = armed
 
 target_ips = ['IP_ADDRESS']
 
 def find_devices(target_ips):
-	icmp_request = IP(dst=target_ips)/ICMP()
+    icmp_request = IP(dst=target_ips)/ICMP()
 
-	icmp_replies, _ = sr(icmp_request, timeout=2, verbose=False)
+    icmp_replies, _ = sr(icmp_request, timeout=2, verbose=False)
 
-	return icmp_replies
+    return icmp_replies
 
 lost_device_time = 0
 currently_armed = False
 while True:
-	icmp_replies = find_devices(target_ips)
-	if icmp_replies:
-		for icmp_reply in icmp_replies:
-			os.system('cls' if os.name == 'nt' else "printf '\033c'")
-			print(f"ICMP reply received from {icmp_reply[1].src}")
-			lost_device_time = 0
-			if currently_armed == True:
-				try:
-					print('Disarming')
-					arm_blink(False)
-					currently_armed = False
-				except:
-			        print('Disarm failed. Trying again.')
-					arm_blink(False)
-					currently_armed = False
-				finally:
-					pass
+    icmp_replies = find_devices(target_ips)
+    if icmp_replies:
+        for icmp_reply in icmp_replies:
+            os.system('cls' if os.name == 'nt' else "printf '\033c'")
+            print(f"ICMP reply received from {icmp_reply[1].src}")
+            lost_device_time = 0
+            if currently_armed == True:
+                try:
+                    print('Disarming')
+                    time.sleep(60)
+                    arm_blink(False)
+                    currently_armed = False
+                except:
+                    print('Disarm failed. Trying again.')
+                    arm_blink(False)
+                    currently_armed = False
+                finally:
+                    pass
 
-			else:
-				pass
+            else:
+                pass
 
-	else:
-		lost_device_time += 1
-		os.system('cls' if os.name == 'nt' else "printf '\033c'")
-		print('Time without connection: '+ str(lost_device_time))
+    else:
+        lost_device_time += 1
+        os.system('cls' if os.name == 'nt' else "printf '\033c'")
+        print('Time without connection: '+ str(lost_device_time))
 
 #Check if devices have been offline for more than 5 minutes. (20)
-	if (lost_device_time == 20) and (currently_armed == False):
-		arm_blink(True)
-		print('Armed')
-		currently_armed = True
-	else:
-		pass
+    if (lost_device_time == 20) and (currently_armed == False):
+        arm_blink(True)
+        print('Armed')
+        currently_armed = True
+    else:
+        pass
 
-	time.sleep(3)
+    time.sleep(3)
